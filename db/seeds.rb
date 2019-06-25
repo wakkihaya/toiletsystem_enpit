0d42094f6b4c2bfbd0f0f1b2f585fa32d29917a3
wakkihaya <wakkihaya@gmail.com>
2019-06-25 23:50:23 +0900

    UIの編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,3 +30,60 @@ Place.create({
                 num: '4',
             }
 )
+
+Place.create({
+                 name: '3B',
+                 latitude: '36.11108',
+                 longitude: '140.10030',
+                 num: '2',
+             }
+)
+
+Place.create({
+                 name: '3D',
+                 latitude: '36.11002',
+                 longitude: '140.09983',
+                 num: '3',
+             }
+)
+
+Place.create({
+                 name: '3E',
+                 latitude: '36.11000',
+                 longitude: '140.09994',
+                 num: '3',
+             }
+)
+
+Place.create({
+                 name: '2D',
+                 latitude: '36.11174',
+                 longitude: '140.10063',
+                 num: '4',
+             }
+)
+
+Place.create({
+                 name: '2C',
+                 latitude: '36.11136',
+                 longitude: '140.10098',
+                 num: '4',
+             }
+)
+
+Place.create({
+                 name: '2B',
+                 latitude: '36.11095',
+                 longitude: '140.10126',
+                 num: '4',
+             }
+)
+
+Place.create({
+                 name: '中央図書館',
+                 latitude: '36.10916',
+                 longitude: '140.10122',
+                 num: '2',
+             }
+)
+
